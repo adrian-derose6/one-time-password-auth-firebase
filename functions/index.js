3def746a0f408d882a85c2ae1b7820bf7f5e7068
Adrian DeRose <adrian.derose@zumer.io>
2017-05-11 03:19:30 -0700

    Setup boilerplate for verifyOneTimePassword

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount.json');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
-const requestOneTimePassword = require('./request_one_time_password')
+const requestOneTimePassword = require('./request_one_time_password');
+const verifyOneTimePassword = require('./verify_one_time_password');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,3 +12,4 @@ admin.initializeApp({
 
 exports.createUser = functions.https.onRequest(createUser);
 exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
