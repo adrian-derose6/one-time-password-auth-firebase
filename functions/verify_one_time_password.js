ca0b566aa8dba1f1f1e4e79a21656b264b334253
Adrian DeRose <adrian.derose@zumer.io>
2017-05-11 03:39:48 -0700

    Check if verification code from user request matches the code from the database

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -1,3 +1,31 @@
-module.exports = function() {
-  
+const admin = require('firebase-admin');
+
+module.exports = function(req, res) {
+  const body = JSON.parse(req.body);
+
+  // Verify that user sent phone number and password
+  if (!body.phone || !body.password) {
+    return res.status(422).send({ error: 'Phone and code must be provided.' });
+  }
+
+  // Format user inputs
+  const phone = String(body.phone).replace(/[^\d]/g, '');
+  const code = parseInt(code);
+
+  // Fetch user model from Firebase Authentification
+  admin.auth().getUser(phone)
+    .then(() => {
+      // Fetch data model corresponding to user
+      admin.database().ref('users/' + phone).on('value', snapshot => {
+        const user = snapshot.val();
+
+        // Check if verification code from user request matches with the code in the database
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: 'Code not valid' });
+        }
+      });
+    })
+    .catch((err) => {
+      res.status(422).send({ error: err });
+    })
 }
