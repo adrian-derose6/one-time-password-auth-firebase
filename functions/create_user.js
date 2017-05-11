bed7ab7dc82c4343e5ba8c64890bad43ebe0dce1
Adrian DeRose <adrian.derose@zumer.io>
2017-05-11 01:07:36 -0700

    Convert JSON response object to JavaScript object for use inside the createUser function

--- functions/create_user.js
+++ functions/create_user.js
@@ -2,12 +2,13 @@ var admin = require('firebase-admin');
 
 module.exports = function(req, res) {
   // Verify the user provided a phone number
-  if (!req.body.phone) {
+  const body = JSON.parse(req.body);
+  if (!body.phone) {
     return res.status(422).send({ error: 'Bad Input' });
   }
 
   // Format the phone number to remove dashes and parens
-  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+  const phone = String(body.phone).replace(/[^\d]/g, "");
 
   // Create a new user account using that phone number
   admin.auth().createUser({ uid: phone })
@@ -15,4 +16,6 @@ module.exports = function(req, res) {
     .catch(err => res.status(422).send({ error: err }));
 
   // Respond to the user request, saying the account was made
+
+
 }
